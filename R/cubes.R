4e9b5af369c5955ef17707928634b4636f81def8
Felipe Carlos <efelipecarlos@gmail.com>
2025-09-09 14:13:32 -0300

    cubes: add function to generate indices in glad cubes

--- R/cubes.R
+++ R/cubes.R
@@ -1,6 +1,6 @@
 
 #' @export
-cube_generate_indices <- function(cube, output_dir, multicores, memsize) {
+cube_generate_indices_bdc <- function(cube, output_dir, multicores, memsize) {
     # Generate NDVI
     cube <- sits_apply(
         data       = cube,
@@ -43,6 +43,52 @@ cube_generate_indices <- function(cube, output_dir, multicores, memsize) {
 
     return(cube)
 }
+
+#' @export
+cube_generate_indices_glad <- function(cube, output_dir, multicores, memsize) {
+    # Generate NDVI
+    cube <- sits_apply(
+        data       = cube,
+        NDVI       = (NIR - RED) / (NIR + RED),
+        output_dir = output_dir,
+        multicores = multicores,
+        memsize    = memsize,
+        progress   = TRUE
+    )
+
+    # Generate EVI (https://www.usgs.gov/landsat-missions/landsat-enhanced-vegetation-index)
+    cube <- sits_apply(
+        data       = cube,
+        EVI        = 2.5 * ((NIR - RED) / (NIR + 6 * RED - 7.5 * BLUE + 1)),
+        output_dir = output_dir,
+        multicores = multicores,
+        memsize    = memsize,
+        progress   = TRUE
+    )
+
+    # Generate MNDWI
+    cube <- sits_apply(
+        data       = cube,
+        MNDWI      = (GREEN - SWIR1) / (GREEN + SWIR1),
+        output_dir = output_dir,
+        multicores = multicores,
+        memsize    = memsize,
+        progress   = TRUE
+    )
+
+    # 1.6. Generate NBR (https://www.usgs.gov/landsat-missions/landsat-normalized-burn-ratio)
+    cube <- sits_apply(
+        data       = cube,
+        NBR        = (NIR - SWIR2) / (NIR + SWIR2),
+        output_dir = output_dir,
+        multicores = multicores,
+        memsize    = memsize,
+        progress   = TRUE
+    )
+
+    return(cube)
+}
+
 
 #' @export
 cube_to_rgb_mosaic <- function(cube,
