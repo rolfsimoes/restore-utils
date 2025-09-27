cf4b07c363cbc998098fe804c7d7da3be944456c
Felipe Carlos <efelipecarlos@gmail.com>
2025-09-27 11:03:51 -0300

    dropbox: utility function to download temporal files

--- R/dropbox.R
+++ R/dropbox.R
@@ -16,3 +16,33 @@ dropbox_upload <- function(files, dropbox_dir) {
 dropbox_dir <- function(name) {
     .dropbox_default_folder() / name
 }
+
+#' @export
+dropbox_download_temporal_files <- function(output_dir, files, years) {
+    # Define output dir
+    output_dir <- fs::path(output_dir) / years
+
+    # Create output directory
+    fs::dir_create(output_dir)
+
+    # Prepare urls
+    files <- stringr::str_replace(files, "&dl=0", "&dl=1")
+
+    # Prepare file names
+    filenames <- basename(sub("\\?.*$", "", files))
+    destfiles <- fs::path(output_dir, filenames)
+
+    # Download the files
+    purrr::walk2(files, destfiles, function(file, destfile) {
+        # If file exist, just return it
+        if (fs::file_exists(destfile)) {
+            return(destfile)
+        }
+
+        # Download file
+        download.file(file, destfile)
+    })
+
+    # Return files
+    destfiles
+}
