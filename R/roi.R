136af8ae19841b0f9859a59058a21dbe90729d7b
Felipe Carlos <efelipecarlos@gmail.com>
2025-10-06 14:58:10 -0300

    roi: add amazon biome as roi

--- R/roi.R
+++ R/roi.R
@@ -1,7 +1,13 @@
 .roi_ecoregion_data_file <- function() {
     default_file <- system.file("extdata/amazon/amazon-regions-bdc-md.gpkg", package = "restoreutils")
 
-    fs::path(Sys.getenv("RESTORE_PLUS_REG3_ROI_FILE", default_file))
+    fs::path(Sys.getenv("RESTORE_PLUS_ROI_FILE", default_file))
+}
+
+.roi_amazon_biome_data_file <- function() {
+    default_file <- system.file("extdata/amazon/amazon-biome.gpkg", package = "restoreutils")
+
+    fs::path(Sys.getenv("RESTORE_PLUS_ROI_FILE", default_file))
 }
 
 .roi_ecoregion_name <- function(region_id) {
@@ -19,6 +25,11 @@
     eco_region <- sf::st_transform(eco_region, crs = crs)
     eco_region <- dplyr::filter(eco_region, layer == eco_name) |>
         dplyr::select(-gid, -id, -grs_schema)
+}
+
+.roi_amazon_biome_sf <- function() {
+    # Load ecoregion roi file
+    eco_region <- sf::st_read(.roi_amazon_biome_data_file(), quiet = TRUE)
 }
 
 #' @export
@@ -61,6 +72,40 @@ roi_ecoregions <- function(region_id, crs, as_file = FALSE, as_union = FALSE, as
     return(eco_region_geom)
 }
 
+#' @export
+roi_amazon_biome <- function(as_line = TRUE, as_file = FALSE, as_convex = FALSE, use_buffer = FALSE) {
+    # generate eco region geometry
+    amazon_geom <- .roi_amazon_biome_sf()
+
+    if (as_line) {
+        amazon_geom <- sf::st_boundary(amazon_geom)
+    }
+
+    # apply buffer if required
+    if (!as_line && use_buffer) {
+        amazon_geom <- sf::st_buffer(amazon_geom, 0.001)
+    }
+
+    # transform convex hull
+    if (!as_line && as_convex) {
+        amazon_geom <- sf::st_union(amazon_geom) |>
+            sf::st_convex_hull()
+    }
+
+    if (as_file) {
+        # create temp file
+        amazon_file <- fs::file_temp(ext = "gpkg")
+
+        # save sf
+        sf::st_write(amazon_geom, amazon_file, quiet = TRUE)
+
+        # update result variable
+        amazon_geom <- amazon_file
+    }
+
+    # return!
+    return(amazon_geom)
+}
 
 #' @export
 crop_to_roi <- function(cube, tiles, multicores, output_dir, grid_system = "BDC_MD_V2") {
