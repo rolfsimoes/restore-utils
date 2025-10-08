17abd0b708eb6b92f43549bc53c0808dea0f405e
Felipe Carlos <efelipecarlos@gmail.com>
2025-10-08 19:42:32 -0300

    reclassify: add new urban rules

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -66,6 +66,30 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// C_trajectory_neighbor_consistency_analysis
+NumericMatrix C_trajectory_neighbor_consistency_analysis(NumericMatrix data, int reference_class);
+RcppExport SEXP _restoreutils_C_trajectory_neighbor_consistency_analysis(SEXP dataSEXP, SEXP reference_classSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< NumericMatrix >::type data(dataSEXP);
+    Rcpp::traits::input_parameter< int >::type reference_class(reference_classSEXP);
+    rcpp_result_gen = Rcpp::wrap(C_trajectory_neighbor_consistency_analysis(data, reference_class));
+    return rcpp_result_gen;
+END_RCPP
+}
+// C_trajectory_neighbor_majority_analysis
+NumericMatrix C_trajectory_neighbor_majority_analysis(NumericMatrix data, int reference_class);
+RcppExport SEXP _restoreutils_C_trajectory_neighbor_majority_analysis(SEXP dataSEXP, SEXP reference_classSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< NumericMatrix >::type data(dataSEXP);
+    Rcpp::traits::input_parameter< int >::type reference_class(reference_classSEXP);
+    rcpp_result_gen = Rcpp::wrap(C_trajectory_neighbor_majority_analysis(data, reference_class));
+    return rcpp_result_gen;
+END_RCPP
+}
 // C_trajectory_urban_analysis
 NumericMatrix C_trajectory_urban_analysis(NumericMatrix data, NumericMatrix mask, int urban_class_id, int forest_class_id, int forest_class_id_mask);
 RcppExport SEXP _restoreutils_C_trajectory_urban_analysis(SEXP dataSEXP, SEXP maskSEXP, SEXP urban_class_idSEXP, SEXP forest_class_idSEXP, SEXP forest_class_id_maskSEXP) {
@@ -87,6 +111,8 @@ static const R_CallMethodDef CallEntries[] = {
     {"_restoreutils_C_remap_values", (DL_FUNC) &_restoreutils_C_remap_values, 3},
     {"_restoreutils_C_trajectory_transition_analysis", (DL_FUNC) &_restoreutils_C_trajectory_transition_analysis, 3},
     {"_restoreutils_C_trajectory_neighbor_analysis", (DL_FUNC) &_restoreutils_C_trajectory_neighbor_analysis, 3},
+    {"_restoreutils_C_trajectory_neighbor_consistency_analysis", (DL_FUNC) &_restoreutils_C_trajectory_neighbor_consistency_analysis, 2},
+    {"_restoreutils_C_trajectory_neighbor_majority_analysis", (DL_FUNC) &_restoreutils_C_trajectory_neighbor_majority_analysis, 2},
     {"_restoreutils_C_trajectory_urban_analysis", (DL_FUNC) &_restoreutils_C_trajectory_urban_analysis, 5},
     {NULL, NULL, 0}
 };
