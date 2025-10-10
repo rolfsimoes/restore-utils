bf262bc97c39f371fe248ab1750cbbce3939c600
Felipe <lipecaso@gmail.com>
2025-10-10 19:48:02 +0000

    update global options

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,19 @@
 #' @importFrom Rcpp sourceCpp
 #' @useDynLib restoreutils, .registration = TRUE
 NULL
+
+options(
+    "download.file.extra" = c(
+        "-L",                    # follow redirects
+        "--fail",                # non-2xx => error
+        "--retry", "20",         # retry count
+        "--remove-on-error",     # remove file on error
+        #"--skip-existing",       # skipping when downloaded file name exists
+        "--retry-delay", "60",   # seconds between retries
+        "--retry-all-errors",    # retry on all failure types
+        "--retry-connrefused",
+        "--connect-timeout", "15",
+        "--max-time", "720",
+        "--verbose"
+    )
+)
