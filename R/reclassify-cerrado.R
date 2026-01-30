038a28acf4c0bf3bdf401158d9a16f74ddce1361
Felipe <lipecaso@gmail.com>
2026-01-30 22:28:40 +0100

    R: fix setdiff call

--- R/reclassify-cerrado.R
+++ R/reclassify-cerrado.R
@@ -10,7 +10,7 @@ reclassify_cer_rule0_natveg <- function(cube, mask, multicores, memsize, output_
 
     # Filter labels to be included in mask
     labels_to_mask <- setdiff(
-        not_included_labels, unname(sits::sits_labels(cube))
+        unname(sits::sits_labels(cube)), not_included_labels
     )
 
     # Build rules expression: each label will be a class
