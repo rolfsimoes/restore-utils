529906430eb046ec6998f0f8928217694c9d87bc
Felipe Carlos <efelipecarlos@gmail.com>
2026-02-06 02:11:43 -0300

    na-utils: add function to detect chunks with na
    Co-authored-by: Felipe Carvalho <lipecaso@gmail.com>

--- R/na_utils.R
+++ R/na_utils.R
@@ -423,3 +423,111 @@ contextual_cleaner <- function(cube,
         update_bbox = FALSE
     )
 }
+
+#' @export
+detect_chunks_with_na <- function(files, version, multicores, memsize, output_dir) {
+    # Create output directory
+    output_dir <- fs::path(output_dir)
+    fs::dir_create(output_dir)
+    # Define output file
+    out_filename <- .reclassify_perene_filename(version)
+    out_file <- fs::path(output_dir) / out_filename
+    # If result already exists, return it!
+    if (file.exists(out_file)) {
+        return(out_file)
+    }
+    # The following functions define optimal parameters for parallel processing
+    rast_template <- sits:::.raster_open_rast(files)
+    image_size <- list(
+        nrows = sits:::.raster_nrows(rast_template),
+        ncols = sits:::.raster_ncols(rast_template)
+    )
+    # Get block size
+    block <- sits:::.raster_file_blocksize(sits:::.raster_open_rast(files))
+    # Check minimum memory needed to process one block
+    job_block_memsize <- sits:::.jobs_block_memsize(
+        block_size = sits:::.block_size(block = block, overlap = 0),
+        npaths = length(files),
+        nbytes = 8,
+        proc_bloat = sits:::.conf("processing_bloat")
+    )
+    # Update multicores parameter based on size of a single block
+    multicores <- sits:::.jobs_max_multicores(
+        job_block_memsize = job_block_memsize,
+        memsize = memsize,
+        multicores = multicores
+    )
+    # Fixed block
+    block <- c(
+        nrows = 2048,
+        ncols = 2048
+    )
+    # Create chunks
+    chunks <- sits:::.chunks_create(
+        block = block,
+        overlap = 0,
+        image_size = image_size,
+        image_bbox = sits:::.bbox(
+            sits:::.raster_bbox(rast_template),
+            default_crs = terra::crs(rast_template)
+        )
+    )
+    # Update chunk to save extra information
+    chunks[["files"]] <- rep(list(files), nrow(chunks))
+    # Start workers
+    sits:::.parallel_start(workers = multicores)
+    on.exit(sits:::.parallel_stop(), add = TRUE)
+    # Process data!
+    block_files <- sits:::.jobs_map_parallel_chr(chunks, function(chunk) {
+        # Get chunk block
+        block <- sits:::.block(chunk)
+        # Get extra context defined by restoreutils
+        files <- chunk[["files"]][[1]]
+        # Define block file name / path
+        block_file <- sits:::.file_block_name(
+            pattern = tools::file_path_sans_ext(out_filename),
+            block = block,
+            output_dir = output_dir
+        )
+        # If block already exists, return it!
+        if (file.exists(block_file)) {
+            return(block_file)
+        }
+        # Read raster values
+        values <- sits:::.raster_read_rast(files = files, block = block)
+        # If there is any NA, save chunk
+        if (any(is.na(values))) {
+            # Prepare and save results as raster
+            sits:::.raster_write_block(
+                files = block_file,
+                block = block,
+                bbox = sits:::.bbox(chunk),
+                values = values,
+                data_type = "INT1U",
+                missing_value = 255,
+                crop_block = NULL
+            )
+            # Returned block file
+            return(block_file)
+        }
+        # Free memory
+        gc()
+        # Return
+        NULL
+    }, progress = TRUE)
+    # Remove NA values
+    block_files <- block_files[!is.na(block_files)]
+    # Merge raster blocks
+    sits:::.raster_merge_blocks(
+        out_files = out_file,
+        base_file = files,
+        block_files = block_files,
+        data_type = "INT1U",
+        missing_value = 255,
+        multicores = multicores
+    )
+    # Remove block files
+    unlink(block_files)
+    # Return!
+    return(out_file)
+}
